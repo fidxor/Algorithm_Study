5234f7aafbee1cfc7ff19fadbf9c609eadf8b48d
fidxor <130888485+fidxor@users.noreply.github.com>
2023-07-07 12:45:19 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 568 ms, Memory: 115284 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
@@ -0,0 +1,23 @@
+lst = list(map(int, input().split()))
+
+def BubbleSort(N, K):
+    numlst = list(map(int, input().split()))
+    resultlst = []
+
+    lastindex = N - 1
+    cnt = 0
+
+    for last in numlst:
+        for i in range(lastindex):
+            if numlst[i] > numlst[i + 1]:
+                numlst[i], numlst[i +1] = numlst[i +1], numlst[i]
+                cnt += 1
+                if cnt == K:
+                    resultlst = [numlst[i], numlst[i +1]]
+                    print(*resultlst)
+                    return
+                
+    if cnt < K:
+        print(-1)
+
+BubbleSort(lst[0], lst[1])
