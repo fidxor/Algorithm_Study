61ae8a5cae15454573e40cc2b477c4308a1a882a
fidxor <fidxordl5404@gmail.com>
2023-08-06 14:51:42 +0900

    Add 플레이데이터 1차 코딩테스트 풀이

---
+++ DataStructureStudy/1_playdatacodingtest.py
@@ -0,0 +1,61 @@
+# user_input = input()
+# userCards = list(map(int, input().split()))
+
+# cards = dict()
+
+
+### 2번 문제
+# for card in userCards:
+#     if card not in cards:
+#         cards[card] = 1
+#     else:
+#         cards[card] += 1
+
+# cards = dict(sorted(cards.items(), key=lambda x : x[1]))
+
+# print(list(cards.keys())[0])
+
+
+### 3번 문제
+import sys
+import datetime
+N, K = map(int, input().split())
+
+timeDict = dict()
+
+for i in range(N):
+    inoutTime = sys.stdin.readline().rstrip()
+    name, time = inoutTime.split()
+    time = datetime.datetime.strptime(time, '%H:%M')    
+    timelst = []
+    timelst.append(time)
+
+    if name not in timeDict:
+        timeDict[name] = timelst
+    else:
+        timeDict[name] += timelst
+
+
+for times in timeDict.values():
+    totaltime = 0    
+    for i in range(0, len(times), 2):
+        time = times[i + 1] - times[i]
+        totaltime += time.seconds / 60 / 60
+    
+    print(totaltime)
+    
+
+### 4번 문제
+# import sys
+
+# N, K = map(int, input().split())
+
+# wordlst = list()
+
+# for i in range(N):
+#     word = sys.stdin.readline().rstrip()
+#     wordlst.append(word)
+
+# wordlst = sorted(wordlst, key=lambda x : (len(x), x))
+
+# print(wordlst[K - 1])
--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -3,25 +3,23 @@ def solution(genres, plays):
 
     songDict = dict()    
 
-    # plays를 인덱스와 묶어서 리스트로 만든다
-    # output : [(500, 0), (600, 1), (150, 2), (800, 3), (2500, 4)]
+    # plays를 인덱스와 묶어서 리스트로 만든다   
     testlst = [(v, i) for i, v in enumerate(plays)]
+     # output : [(500, 0), (600, 1), (150, 2), (800, 3), (2500, 4)]
     
     # plays와 인덱스를 묶은 리스트를 genres로 다시 묶어서 리스트를 만든다
+    testlst = list(zip(genres, testlst))
     # output : [('classic', (500, 0)), ('pop', (600, 1)), ('classic', (150, 2)), ('classic', (800, 3)), ('pop', (2500, 4))]
-    testlst = list(zip(genres, testlst))     
-    
 
-    # 위에서 만든 리스트를 가지고 genres 값을 key로 잡아 dictionary를 만든다.
-    # output : {'classic': [(500, 0), (150, 2), (800, 3)], 'pop': [(600, 1), (2500, 4)]}
+    # 위에서 만든 리스트를 가지고 genres 값을 key로 잡아 dictionary를 만든다.    
     for i in range(len(genres)):       
         if testlst[i][0] in songDict:
             songDict[testlst[i][0]] += [testlst[i][1]]
         else:
-            songDict[testlst[i][0]] = [testlst[i][1]]    
+            songDict[testlst[i][0]] = [testlst[i][1]]
+    # output : {'classic': [(500, 0), (150, 2), (800, 3)], 'pop': [(600, 1), (2500, 4)]}
     
-    # 각 key의 value로 들어있는 리스트에 plays만 더해줘서 리스트의 마지막에 넣어준다.
-    # output : {'classic': [(800, 3), (500, 0), (150, 2), 1450], 'pop': [(2500, 4), (600, 1), 3100]}
+    # 각 key의 value로 들어있는 리스트에 plays만 더해줘서 리스트의 마지막에 넣어준다.    
     for i in songDict:
         temp = 0
         songDict[i].sort(key=lambda x: x[0], reverse=True) # 리스트 더해주기전에 plays 내림차순으로 정렬
@@ -29,7 +27,9 @@ def solution(genres, plays):
             temp += j[0]            
         
         songDict[i] += list([temp])
+    # output : {'classic': [(800, 3), (500, 0), (150, 2), 1450], 'pop': [(2500, 4), (600, 1), 3100]}
 
+    
     # 리스트 마지막에 더해준 값을 기준으로 dictionary key를 정렬시켜준다
     songDict = dict(sorted(songDict.items(), key=lambda x : x[1][-1], reverse=True))
 
