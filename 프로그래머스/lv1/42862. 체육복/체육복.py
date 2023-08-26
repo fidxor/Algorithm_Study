69c489e2bd2fbc66b8ae995388fac52feb9204bb
fidxor <130888485+fidxor@users.noreply.github.com>
2023-08-26 17:23:51 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,9 +1,9 @@
 def solution(n, lost, reserve):
     answer = 0
-    
+
     lost.sort()
-    reserve.sort()
-    
+    reserve.sort()    
+
     lostcopy = lost.copy()
 
     for i in lost:
@@ -15,9 +15,11 @@ def solution(n, lost, reserve):
     lostDuplicate = lostcopy.copy()
 
     for i in lostcopy:
-        back = i - 1
-        forward = i + 1
+        back = i - 1    # 잃어버린 학생 번호의 뒷번호
+        forward = i + 1 # 잃어버린 학생 번호의 앞번호 
 
+        # back 이나 forward 번호가 reserve 리스트에 있으면 
+        # lost와 reserve에서 지워준다.
         if back in reserve:
             if i in lostDuplicate: lostDuplicate.remove(i)
             if back in reserve: reserve.remove(back)
@@ -25,6 +27,7 @@ def solution(n, lost, reserve):
             if i in lostDuplicate: lostDuplicate.remove(i)
             if forward in reserve: reserve.remove(forward)
 
+    # 총원에서 끝까지 체육복을 빌리지 못한 인원수를 빼준다.
     answer = n - len(lostDuplicate)
-    
-    return answer
+
+    return answer 
